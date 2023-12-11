73cb6c34de614761e432201a7b770dc2d0f4299d
fernargdev <115408504+fernargdev@users.noreply.github.com>
2023-12-10 19:00:01 -0500

    filters-fc

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,3 +1,4 @@
+import { FILTERS_BUTTONS } from '../consts'
 import { FilterValue } from '../types'
 
 interface Props {
@@ -8,7 +9,24 @@ interface Props {
 const Filters: React.FC<Props> = ({ filterSelected, onFilterChange }) => {
   return (
     <ul className="filters">
-      <li></li>
+      {Object.entries(FILTERS_BUTTONS).map(([key, { href, literal }]) => {
+        const isSelected = key === filterSelected
+        const className = isSelected ? 'selected' : ''
+
+        return (
+          <li key={key}>
+            <a
+              href={href}
+              className={className}
+              onClick={(event) => {
+                event.preventDefault()
+                onFilterChange(key as FilterValue)
+              }}
+            />
+            {literal}
+          </li>
+        )
+      })}
     </ul>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,32 @@
+import { FilterValue } from '../types'
 import Filters from './Filters'
 
-const Footer: React.FC<Props> = ({ activeCount, todos, onClearCompleted }) => {
+interface Props {
+  activeCount: number
+  completeCount: number
+  filterSelected: FilterValue
+  onClearCompleted: () => void
+  handleFilterChange: (filter: FilterValue) => void
+}
+
+const Footer: React.FC<Props> = ({
+  activeCount = 0,
+  completeCount = 0,
+  filterSelected,
+  onClearCompleted,
+  handleFilterChange,
+}) => {
   return (
     <footer className="footer">
       <span className="todo-count">
-        <strong>{todos.length}</strong>
+        <strong>{activeCount}</strong>
         Tareas Pendientes
       </span>
 
-      <Filters filterSelect={} onFilterSelected={} onFilterChange={} />
+      <Filters
+        filterSelected={filterSelected}
+        onFilterChange={handleFilterChange}
+      />
     </footer>
   )
 }
